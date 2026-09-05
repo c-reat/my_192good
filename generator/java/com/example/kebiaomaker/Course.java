34bb7c0ef68f7b70d0e346318304335c1391c207
c-reat <rasulovqobil4@gmail.com>
2026-09-05 09:43:33 +0800

    添加内农大课程表项目源码

---
+++ generator/java/com/example/kebiaomaker/Course.java
@@ -0,0 +1,12 @@
+package com.example.kebiaomaker;
+
+public class Course {
+    public String name = "";
+    public String place = "";
+    public String teacher = "";
+    public int start = 1;
+    public int end = 17;
+    public int parity = 0; // 0=每周 1=单周 2=双周
+    public boolean isLab = false;
+    public boolean multiTeacher = false;
+}
